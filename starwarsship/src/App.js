e14612f42b62fc4456a3aefc8c993a784048c6fb
Banu Seyhan <banuseyhan@Banu-MacBook-Pro.local>
2022-09-26 23:54:14 +0300

    improvement has been made

--- starwarsship/src/App.js
+++ starwarsship/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home/Home";
+import { Route } from "react-router-dom";
+import Starships from "./components/Starships/Starships";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Home} />
+      <Route
+        path="/Starships"
+        render={({ location }) => <Starships location={location} />}
+      />
+     
     </div>
   );
 }
