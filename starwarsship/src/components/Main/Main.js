7f232478d931ebe487468714d91d36d33ef28b37
Banu Seyhan <banuseyhan@Banu-MacBook-Pro.local>
2022-09-29 11:53:14 +0300

    Main done

--- starwarsship/src/components/Main/Main.js
+++ starwarsship/src/components/Main/Main.js
@@ -8,11 +8,14 @@ export const Main = () => {
         <div className="container main__container">
           <div className="main__description">
             <p className="main__description-text">
-            A long time ago in a galaxy far, far away.. this is how the story begins. But it has become a philosophical view, not just a story. 
-            In fact, we all have a dark side inside us. According to this philosophy, our dark sides are actually a tool that tries to make us good and helps us choose the right paths.
-             In these stories, many starships are used to reach new hopes and new excitements. 
-             If you are wondering, you can reach starships from the starhips tab. 
-             May the force be with you
+              A long time ago in a galaxy far, far away.. this is how the story
+              begins. But it has become a philosophical view, not just a story.
+              In fact, we all have a dark side inside us. According to this
+              philosophy, our dark sides are actually a tool that tries to make
+              us good and helps us choose the right paths. In these stories,
+              many starships are used to reach new hopes and new excitements. If
+              you are wondering, you can reach starships from the starhips tab.
+              May the force be with you
             </p>
           </div>
         </div>
